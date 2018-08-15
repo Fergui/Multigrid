1b46e81c48c87a56a653d0144cdb3bf1ccb76c17
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-15 08:57:37 -0600

    Update binsearch_bounds.m

--- src/optim/binsearch_bounds.m
+++ src/optim/binsearch_bounds.m
@@ -1,16 +1,16 @@
 function ii=binsearch_bounds(s,smin,smax)
 % Call:
 % ii=binsearch_bounds(s,smin,smax)
-
+%
 % Description:
 % Search the array of all indices such that smin <= s(i) <= smax
-
+%
 % Inputs:
 %   s           array sorted in increasing order
 %   smin,smax   bounds to select values of xs
 % Outputs:
 %   ii          array of all indices such that smin <= s(i) <= smax
-
+%
 %-------------------------------------------------------------------------
 
 imin=binsearch_leq(s,smin);
