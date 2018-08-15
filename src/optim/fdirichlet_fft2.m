b08f79853046367f24c56189b1d0302ad8751386
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-15 08:54:59 -0600

    Update fdirichlet_fft2.m

--- src/optim/fdirichlet_fft2.m
+++ src/optim/fdirichlet_fft2.m
@@ -1,14 +1,18 @@
 function U = fdirichlet_fft2(r,h,f)
+% Call:
 % v=fdirichlet_fft2(r,h,f)
-% Multiply by a function of minus Laplace operator with dirichlet zero
-% boundary conditions by sine FFT
 %
-% in:
+% Description:
+% Multiply by a function of minus Laplace operator with dirichlet zero boundary conditions by sine FFT.
+%
+% Inputs:
 %   r   2d gridded array
 %   h   vector size 2: grid spacing
 %   f   function, must work entry by entry for matrix matrix argument 
-% out:
+% Outputs:
 %   u = f(-d^2/dx^2 - d^2/dy^2)*r
+%
+%-------------------------------------------------------------------------
 
 % with [N1,N2]=size(r) on the square [0 (N1+1)*h(1)] times [0 (N2+1)*h(2)]
 % return values at nodes [i1*h(1),i2*h(2)], i1=1:N1, i2=1:N2
