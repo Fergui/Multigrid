81496dc77b0b87791ab54118bbe183715245e543
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-16 14:59:05 -0600

    Add files via upload

---
+++ src/utils/kml2struct.m
@@ -0,0 +1,138 @@
+function kmlStruct = kml2struct(kmlFile)
+% kmlStruct = kml2struct(kmlFile)
+%
+% Import a .kml file as a vector array of shapefile structs, with Geometry, Name,
+% Description, Lon, Lat, and BoundaryBox fields.  Structs may contain a mix
+% of points, lines, and polygons.
+%
+% .kml files with folder structure will not be presented as such, but will
+% appear as a single vector array of structs.
+%
+% 
+
+%Downloaded from https://www.mathworks.com/matlabcentral/fileexchange/35642-kml2struct
+%October 13, 2016
+%
+%Copyright (c) 2012, James Slegers
+%All rights reserved.
+%
+%Redistribution and use in source and binary forms, with or without
+%modification, are permitted provided that the following conditions are
+%met:
+%
+%* Redistributions of source code must retain the above copyright
+%notice, this list of conditions and the following disclaimer.
+%* Redistributions in binary form must reproduce the above copyright
+%notice, this list of conditions and the following disclaimer in
+%the documentation and/or other materials provided with the distribution
+%
+%THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%                       SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%                       INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%POSSIBILITY OF SUCH DAMAGE.
+
+[FID msg] = fopen(kmlFile,'rt');
+
+if FID<0
+    error(msg)
+end
+
+txt = fread(FID,'uint8=>char')';
+fclose(FID);
+
+expr = '<Placemark.+?>.+?</Placemark>';
+
+objectStrings = regexp(txt,expr,'match');
+
+Nos = length(objectStrings);
+
+for ii = 1:Nos
+    % Find Object Name Field
+    bucket = regexp(objectStrings{ii},'<name.*?>.+?</name>','match');
+    if isempty(bucket)
+        name = 'undefined';
+    else
+        % Clip off flags
+        name = regexprep(bucket{1},'<name.*?>\s*','');
+        name = regexprep(name,'\s*</name>','');
+    end
+    
+    % Find Object Date Field
+    bucket = regexp(objectStrings{ii},'<TimeSpan.*?>.+?</TimeSpan>','match');
+    if isempty(bucket)
+        date = 'undefined';
+    else
+        % Clip off flags
+        date = regexprep(bucket{1},'<TimeSpan.*?><begin>\s*','');
+        date = regexprep(date,'\s*</begin></TimeSpan>','');
+    end
+    
+    % Find Object Description Field
+    bucket = regexp(objectStrings{ii},'<description.*?>.+?</description>','match');
+    if isempty(bucket)
+        desc = '';
+    else
+        % Clip off flags
+        desc = regexprep(bucket{1},'<description.*?>\s*','');
+        desc = regexprep(desc,'\s*</description>','');
+    end
+    
+    geom = 0;
+    % Identify Object Type
+    if ~isempty(regexp(objectStrings{ii},'<Point', 'once'))
+        geom = 1;
+    elseif ~isempty(regexp(objectStrings{ii},'<LineString', 'once'))
+        geom = 2;
+    elseif ~isempty(regexp(objectStrings{ii},'<Polygon', 'once'))
+        geom = 3;
+    end
+    
+    switch geom
+        case 1
+            geometry = 'Point';
+        case 2
+            geometry = 'Line';
+        case 3
+            geometry = 'Polygon';
+        otherwise
+            geometry = '';
+    end
+    
+    % Find Coordinate Field
+    bucket = regexp(objectStrings{ii},'<coordinates.*?>.+?</coordinates>','match');
+    for jj = 1:length(bucket)
+       coordStr = regexprep(bucket{jj},'<coordinates.*?>(\s+)*','');
+       coordStr = regexprep(coordStr,'(\s+)*</coordinates>','');
+       coordMat = str2double(regexp(coordStr,'[,\s]+','split'));
+       [m,n] = size(coordMat);
+       coordMat = reshape(coordMat,3,m*n/3)';
+       bucket{jj} = coordMat;
+    end
+
+    % Create structure
+    kmlStruct(ii).Geometry = geometry;
+    kmlStruct(ii).Date = date;
+    kmlStruct(ii).Name = name;
+    kmlStruct(ii).Description = desc;
+    kmlStruct(ii).Lon = cell(1,length(bucket));
+    kmlStruct(ii).Lat = cell(1,length(bucket));
+    kmlStruct(ii).BoundingBox = cell(1,length(bucket));
+    for jj = 1:length(bucket)
+        coord=bucket{jj};
+        [Lat, Lon] = poly2ccw(coord(:,2),coord(:,1));
+        if geom==3
+            Lon = [Lon;NaN];
+            Lat = [Lat;NaN];
+        end
+        kmlStruct(ii).Lon{jj} = Lon;
+        kmlStruct(ii).Lat{jj} = Lat;
+        kmlStruct(ii).BoundingBox{jj} = [[min(Lon) min(Lat);max(Lon) max(Lat)]];
+    end
+end
