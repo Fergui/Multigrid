2ff7c9eb861ccf619ca02743bdf17a40310776eb
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-16 14:27:27 -0600

    Update dispvarinfo.m

--- src/netcdf/dispvarinfo.m
+++ src/netcdf/dispvarinfo.m
@@ -1,5 +1,10 @@
 function dispvarinfo(p)
+% dispvarinfo(p)
 % display info on one variable
+% 
+% Jan Mandel
+%-------------------------------------------------------------------------
+
     if isfield(p,'var_value'),
         disp([p.varname,' size ',num2str(size(p.var_value)),' netcdf stored as ',...
               p.vartype_m,' (',num2str(p.dimlength),')'])
