049cd6dfcf9f3a6997068656d0e05e28bb9251c5
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-20 21:55:54 -0600

    Update plot_sol_mesh.m

--- src/plot/plot_sol_mesh.m
+++ src/plot/plot_sol_mesh.m
@@ -1,12 +1,22 @@
 function plot_sol_mesh(X,Y,u,H,g)
-%plot_sol_mesh(u,H,g)
+% Call:
+% plot_sol_mesh(u,H,g)
+%
+% Description:
 % Plot the fire arrival time and the constraints in a 3D contour and mesh
-%in
+%
+% Inputs:
 %   X       matrix of X coordinates (longitude), or number of points in X direction
 %   Y       matrix of Y coordinates (latitude), or number of points in Y direction
 %   u   fire arrival time over the domain
 %   H   matrix of the constraints Hu=g
 %   g   right hand side of the constraints Hu=g
+% Outputs:
+%   3D contour plot of the solution u and the constraints Hu=g
+% 
+% Developed in Matlab 9.2.0.556344 (R2017a) on MACINTOSH. 
+% Angel Farguell (angel.farguell@gmail.com), 2018-08-15
+%-------------------------------------------------------------------------
 
 [nx,ny]=size(X);
 if nx==1,
@@ -23,7 +33,6 @@ for k=1:length(uu)
     gs=g==uu(k);
     us=H'*gs;
     us=reshape(us,size(u));
-    %contour3(flip((uu(2)*2)*(us>0)),[uu(2)*2,uu(2)*2],col(k)), hold on
     contour3(X,Y,(uu(2))*(us>0),[uu(2),uu(2)],'Color',col(k,:)), hold on
 end
 
