8aad3a59a1f3c504ae7241424594c1fd25f95bb8
Angel Farguell Caus <angel.farguell@gmail.com>
2018-08-20 21:52:49 -0600

    Update plot_sol.m

--- src/plot/plot_sol.m
+++ src/plot/plot_sol.m
@@ -1,10 +1,20 @@
 function plot_sol(u,H,g)
-%plot_sol(u,H,g)
+% Call:
+% plot_sol(u,H,g)
+%
+% Description:
 % Plot the fire arrival time and the constraints in a 3D contour
-%in
+%
+% Inputs:
 %   u   fire arrival time over the domain
 %   H   matrix of the constraints Hu=g
 %   g   right hand side of the constraints Hu=g
+% Outputs:
+%   The result is a 3D plot of the solution u with the constraints Hu=g.
+%
+% Developed in Matlab 9.2.0.556344 (R2017a) on MACINTOSH. 
+% Angel Farguell (angel.farguell@gmail.com), 2018-08-15
+%-------------------------------------------------------------------------
 
 col=['-g','-b','-r'];
 uu=unique(g);
@@ -16,7 +26,6 @@ for k=1:length(uu)
 end
 
 contour3(flip(u),[uu(1):(uu(end)-uu(1))/10:uu(end)],'k')
-%contour3(flip(u),200,'k')
 
 end
 
