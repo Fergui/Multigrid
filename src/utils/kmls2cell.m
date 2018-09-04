cbbb577b1e5158ab903e7177b8697161db3c755e
Angel Farguell Caus <angel.farguell@gmail.com>
2018-09-04 11:15:21 -0600

    Update kmls2cell.m

--- src/utils/kmls2cell.m
+++ src/utils/kmls2cell.m
@@ -1,5 +1,18 @@
 function kmlcell = kmls2cell(regex)
-% From a regex of kml files, it creates a cell of structs
+% Call:
+% kmlcell = kmls2cell(regex)
+%
+% Description:
+% From a regex of kml files, it creates a cell of structs including all the kml files.
+%
+% Inputs:
+%   regex     path to a kml files using a regex expresion
+% Outputs:
+%   kmlcell   kml cell of structs for each kml
+%
+% Developed in Matlab 9.2.0.556344 (R2017a) on MACINTOSH. 
+% Angel Farguell (angel.farguell@gmail.com), 2018-09-04
+%-------------------------------------------------------------------------
 
 kmls=dir(regex);
 nn=length(kmls);
