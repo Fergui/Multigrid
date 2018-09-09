ac688f3f16ba712d8139a708911aaae97c5b8c95
Angel Farguell Caus <angel.farguell@gmail.com>
2018-09-09 12:12:49 -0600

    Update per2mask.m

--- src/interp/per2mask.m
+++ src/interp/per2mask.m
@@ -1,14 +1,22 @@
 function [M,u]=per2mask(H,g,m,n,dx,dy)
-%M=per2mask(H)
+% Call:
+% M=per2mask(H)
+%
+% Description:
 % Define a mask from perimeter defined as constraints Hu=g
-%in 
+%
+% Inputs: 
 %   H      matrix of constraints
 %   g      right hand side of the constraints
 %   m,n    x and y dimensions
 %   dx,dy  x and y resolution
-%out
+% Outputs:
 %   M      mask matrix (1's inside and 0's outside)
 %   u      solution of Dirichlet boundary conditions using constraints
+%
+% Developed in Matlab 9.2.0.556344 (R2017a) on MACINTOSH. 
+% Angel Farguell (angel.farguell@gmail.com), 2018-08-15
+%-------------------------------------------------------------------------
 
 uu=unique(g);
 gs=[m,n];
